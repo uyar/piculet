82da33f3978b2512679ce8c43ee894a11c4533d7
H. Turgut Uyar <uyar@tekir.org>
2017-01-06 20:05:52 +0300

    updated copyright notice for 2017

--- piculet.py
+++ piculet.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2014-2016 H. Turgut Uyar <uyar@tekir.org>
+# Copyright (C) 2014-2017 H. Turgut Uyar <uyar@tekir.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
