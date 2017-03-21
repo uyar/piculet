432bb0de869d44a7d2ad3af64f41a4c7c4aa4040
H. Turgut Uyar <uyar@tekir.org>
2017-03-21 15:16:11 +0300

    regenerated the stub file

--- piculet.pyi
+++ piculet.pyi
@@ -2,6 +2,8 @@
 
 from typing import Any, Callable, Iterable, List, Mapping, Optional, Union
 
+from html.parser import HTMLParser
+
 import xml.etree.ElementTree
 
 
@@ -11,6 +13,7 @@ def retrieve(
         fallback_charset: Optional[str] = ...
 ) -> str: ...
 
+class _HTMLNormalizer(HTMLParser): ...
 
 def html_to_xhtml(
         document: str,
@@ -18,25 +21,21 @@ def html_to_xhtml(
         omit_attrs: Optional[Iterable[str]] = ...
 ) -> str: ...
 
-
 def xpath_etree(
         element: xml.etree.ElementTree.Element,
         path: str
 ) -> Union[List[str], List[xml.etree.ElementTree.Element]]: ...
-
 
 def woodpecker(
         path: str,
         reducer: str
 ) -> Callable[[xml.etree.ElementTree.Element], Optional[str]]: ...
 
-
 def extract(
         document: Union[str, xml.etree.ElementTree.Element],
         items: Iterable[Mapping[str, Any]],
         pre: Optional[Iterable[Mapping[str, Any]]] = ...
 ) -> Mapping[str, Any]: ...
-
 
 def scrape(
         url: str,
