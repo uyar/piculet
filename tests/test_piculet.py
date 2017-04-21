d5e0b792299047c66af746fba3c8f748a86d1181
H. Turgut Uyar <uyar@tekir.org>
2017-04-21 13:57:19 +0300

    Add tests for pre-defined reducers

--- tests/test_piculet.py
+++ tests/test_piculet.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from pytest import raises
 
-from piculet import build_tree, extract, woodpecker, xpath
+from piculet import build_tree, extract, reducers, woodpecker, xpath
 
 
 ########################################
@@ -32,6 +32,44 @@ def test_xpath_descendant_text_queries_should_return_strings():
 def test_xpath_attr_queries_should_return_strings():
     selected = xpath(generic_root, './/t1/@a')
     assert selected == ['v']
+
+
+########################################
+# reducer tests
+########################################
+
+
+def test_reducer_first_should_return_first_item():
+    assert reducers.first(['a', 'b', 'c']) == 'a'
+
+
+def test_reducer_join_should_return_joined_items():
+    assert reducers.join(['a', 'b', 'c']) == 'abc'
+
+
+def test_reducer_clean_should_remove_extra_space():
+    assert reducers.clean(['  a ', '   b', ' c ']) == 'a b c'
+
+
+def test_reducer_clean_should_treat_nbsp_as_space():
+    assert reducers.clean(['  a ', ' \xa0  b', ' c ']) == 'a b c'
+
+
+def test_reducer_normalize_should_convert_to_lowercase():
+    assert reducers.normalize(['A', 'B', 'C']) == 'abc'
+
+
+def test_reducer_normalize_should_remove_nonalphanumeric_characters():
+    assert reducers.normalize(['a+', '?b7', '{c}']) == 'ab7c'
+
+
+def test_reducer_normalize_should_keep_underscores():
+    assert reducers.normalize(['a_', 'b', 'c']) == 'a_bc'
+
+
+def test_reducer_normalize_should_replace_spaces_with_underscores():
+    assert reducers.normalize(['a', ' b', 'c']) == 'a_bc'
+
 
 
 ########################################
