5e327f0beddd9ad476d744a8a93ceda93b2b3693
H. Turgut Uyar <uyar@tekir.org>
2017-05-09 15:05:43 +0300

    Update documentation with sphinx pygenstub extension

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'pygenstub'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
