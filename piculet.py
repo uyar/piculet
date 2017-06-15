3b9e04790dfd1354ed9ecb31c9f25a8df5ea9955
H. Turgut Uyar <uyar@tekir.org>
2017-06-15 14:41:13 +0300

    Make license info consistent

--- piculet.py
+++ piculet.py
@@ -1,17 +1,17 @@
 # Copyright (C) 2014-2017 H. Turgut Uyar <uyar@tekir.org>
 #
-# This program is free software: you can redistribute it and/or modify
+# Piculet is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# Piculet is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with Piculet.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 Piculet is a package for extracting data from XML documents using XPath queries.
