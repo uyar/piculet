3e6f6aef18761b287577a88bc4159af412e28afb
H. Turgut Uyar <uyar@tekir.org>
2018-01-09 14:03:55 +0300

    Move XPath tests to separate module

--- tests/test_piculet.py
+++ tests/test_piculet.py
@@ -2,36 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import os
 
-from piculet import build_tree, extract, reducers, xpath
-
-
-########################################
-# xpath tests
-########################################
-
-
-generic_content = '<d><t1>foo</t1><t1 a="v"><t2>bar</t2></t1></d>'
-generic_root = build_tree(generic_content)
-
-
-def test_xpath_non_text_queries_should_return_nodes():
-    selected = xpath(generic_root, ".//t1")
-    assert [s.tag for s in selected] == ['t1', 't1']
-
-
-def test_xpath_child_text_queries_should_return_strings():
-    selected = xpath(generic_root, ".//t1/text()")
-    assert selected == ['foo']
-
-
-def test_xpath_descendant_text_queries_should_return_strings():
-    selected = xpath(generic_root, ".//t1//text()")
-    assert selected == ['foo', 'bar']
-
-
-def test_xpath_attr_queries_should_return_strings():
-    selected = xpath(generic_root, ".//t1/@a")
-    assert selected == ['v']
+from piculet import build_tree, extract, reducers
 
 
 ########################################
