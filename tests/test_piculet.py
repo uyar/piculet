df80e7642d0578b675418f666da1d3bda9ba400a
H. Turgut Uyar <uyar@tekir.org>
2018-01-09 14:05:32 +0300

    Move reducer tests to separate module

--- tests/test_piculet.py
+++ tests/test_piculet.py
@@ -5,48 +5,6 @@ import os
 from piculet import build_tree, extract, reducers
 
 
-########################################
-# reducer tests
-########################################
-
-
-def test_reducer_first_should_return_first_item():
-    assert reducers.first(['a', 'b', 'c']) == 'a'
-
-
-def test_reducer_concat_should_return_concatenated_items():
-    assert reducers.concat(['a', 'b', 'c']) == 'abc'
-
-
-def test_reducer_clean_should_remove_extra_space():
-    assert reducers.clean(['  a ', '   b', ' c ']) == 'a b c'
-
-
-def test_reducer_clean_should_treat_nbsp_as_space():
-    assert reducers.clean(['  a ', ' \xa0  b', ' c ']) == 'a b c'
-
-
-def test_reducer_normalize_should_convert_to_lowercase():
-    assert reducers.normalize(['A', 'B', 'C']) == 'abc'
-
-
-def test_reducer_normalize_should_remove_nonalphanumeric_characters():
-    assert reducers.normalize(['a+', '?b7', '{c}']) == 'ab7c'
-
-
-def test_reducer_normalize_should_keep_underscores():
-    assert reducers.normalize(['a_', 'b', 'c']) == 'a_bc'
-
-
-def test_reducer_normalize_should_replace_spaces_with_underscores():
-    assert reducers.normalize(['a', ' b', 'c']) == 'a_bc'
-
-
-########################################
-# extraction tests
-########################################
-
-
 shining_file = os.path.join(os.path.dirname(__file__), '..', 'examples', 'shining.html')
 shining = build_tree(open(shining_file).read())
 
