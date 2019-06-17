a001ee90d9447420903696dbfb01c57b2b879f81
H. Turgut Uyar <uyar@tekir.org>
2019-06-17 14:34:35 +0300

    tst: Rename make_xpather to XPath

--- tests/test_xpath.py
+++ tests/test_xpath.py
@@ -1,4 +1,5 @@
-from piculet import build_tree, make_xpather
+from piculet import build_tree
+from piculet import make_xpather as XPath
 
 
 content = '<d><t1>foo</t1><t1 a="v"><t2>bar</t2></t1></d>'
@@ -6,25 +7,25 @@ root = build_tree(content)
 
 
 def test_non_text_queries_should_return_elements():
-    selected = make_xpather(".//t1")(root)
+    selected = XPath(".//t1")(root)
     assert [s.tag for s in selected] == ["t1", "t1"]
 
 
 def test_child_text_queries_should_return_strings():
-    selected = make_xpather(".//t1/text()")(root)
+    selected = XPath(".//t1/text()")(root)
     assert selected == ["foo"]
 
 
 def test_descendant_text_queries_should_return_strings():
-    selected = make_xpather(".//t1//text()")(root)
+    selected = XPath(".//t1//text()")(root)
     assert selected == ["foo", "bar"]
 
 
 def test_attr_queries_should_return_strings():
-    selected = make_xpather(".//t1/@a")(root)
+    selected = XPath(".//t1/@a")(root)
     assert selected == ["v"]
 
 
 def test_non_absolute_queries_should_be_ok():
-    selected = make_xpather("//t1")(root)
+    selected = XPath("//t1")(root)
     assert [s.tag for s in selected] == ["t1", "t1"]
