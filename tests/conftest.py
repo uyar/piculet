798669865b64727f004346600ef2335a0dec9b17
H. Turgut Uyar <uyar@tekir.org>
2023-01-12 19:10:27 +0300

    simplify test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,35 +1,18 @@
-import pytest
+from pytest import fixture
 
 from pathlib import Path
 
 import piculet
 
 
-cache_dir = Path(__file__).parent / ".cache"
-cache_dir.mkdir(exist_ok=True)
-
-examples_dir = Path(__file__).parent.parent / "examples"
-
-
-@pytest.fixture(scope="session")
-def movie_spec():
-    """Path of the spec file movies."""
-    return examples_dir / "movie.json"
-
-
-@pytest.fixture(scope="session")
-def shining_file():
-    """Path of the test file for the movie "The Shining"."""
-    return examples_dir / "shining.html"
-
-
-@pytest.fixture(scope="session")
-def shining_content(shining_file):
+@fixture(scope="session")
+def shining_content():
     """Contents of the test file for the movie "The Shining"."""
-    return shining_file.read_text()
+    examples_dir = Path(__file__).parent.parent / "examples"
+    return examples_dir.joinpath("shining.html").read_text()
 
 
-@pytest.fixture
+@fixture
 def shining(shining_content):
     """Root element of the test XML tree for the movie "The Shining"."""
     return piculet.build_tree(shining_content)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,19 +1,11 @@
 import json
 import subprocess
-
-import piculet
-
-
-def test_help_should_print_usage_and_exit(capfd):
-    subprocess.run(["piculet", "--help"])
-    std = capfd.readouterr()
-    assert std.out.startswith("usage: ")
+from pathlib import Path
 
 
-def test_version_should_print_version_number_and_exit(capfd):
-    subprocess.run(["piculet", "--version"])
-    std = capfd.readouterr()
-    assert std.out == f"{piculet.__version__}\n"
+examples_dir = Path(__file__).parent.parent / "examples"
+movie_spec = examples_dir / "movie.json"
+movie_doc = examples_dir / "shining.html"
 
 
 def test_if_given_no_spec_should_print_usage_and_exit(capfd):
@@ -23,16 +15,17 @@ def test_if_given_no_spec_should_print_usage_and_exit(capfd):
     assert "error: the following arguments are required: -s/--spec" in std.err
 
 
-def test_scrape_should_print_data_extracted_from_stdin(capfd, shining_file, movie_spec):
-    cat = subprocess.run(["cat", shining_file], check=True, capture_output=True)
-    subprocess.run(["piculet", "-s", movie_spec], input=cat.stdout)
+def test_scrape_should_print_data_extracted_from_stdin(capfd):
+    content = movie_doc.read_text()
+    echo = subprocess.run(["echo", content], check=True, capture_output=True)
+    subprocess.run(["piculet", "-s", movie_spec], input=echo.stdout)
     std = capfd.readouterr()
     data = json.loads(std.out)
     assert data["title"] == "The Shining"
 
 
-def test_scrape_should_honor_html_setting(capfd, shining_content, movie_spec):
-    content = shining_content.replace('<meta charset="utf-8"/>', '<meta charset="utf-8">')
+def test_scrape_should_honor_html_setting(capfd):
+    content = movie_doc.read_text().replace('<meta charset="utf-8"/>', '<meta charset="utf-8">')
     echo = subprocess.run(["echo", content], check=True, capture_output=True)
     subprocess.run(["piculet", "-s", movie_spec, "--html"], input=echo.stdout)
     std = capfd.readouterr()
